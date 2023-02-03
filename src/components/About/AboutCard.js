40dd624c44423f3663a7842646d3b2a3fc79524b
Yago <yagomateos@hotmail.com>
2023-02-03 15:01:22 +0100

    Yago-Portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Yago Mateos </span>
+            from <span className="purple"> Barcelona, Spain.</span>
+            <br />I am a Middleweight Front End Developer especialized on Javascript,<br></br> React and MERN stack
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Videogames
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> 3D Animation
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Martial Arts
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Yago</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
